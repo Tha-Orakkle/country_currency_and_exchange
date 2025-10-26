40d26732bacc68278dda321c2628d0f8beaac7d7
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-10-26 12:52:52 +0100

    feat: create endpoint to refresh countries

--- base/views.py
+++ base/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .exceptions import ExternalAPIUnavailable
+from .machine import RefreshCountryMachine
+
+class CountryRefreshView(APIView):
+
+    def post(self, request):
+        
+        machine = RefreshCountryMachine()
+        try:
+            machine.refresh_countries()
+        except ExternalAPIUnavailable as e:
+            return Response({
+                'error': 'External data source unavailable',
+                'details': f'Could not fetch data from {e.api_name}'
+            }, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+            
+        return Response({
+            'status': 'success',
+            'message': 'Countries refreshed successfully.'
+        }, status=status.HTTP_200_OK)
