e52e58a4b6d5354f089f2fc5f59476f1f8e34ce2
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-10-26 15:35:23 +0100

    update: add url pattern for the retrieve and destroy country endpoint

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from base.views import (
     CountryRefreshView,
     CountryListView,
+    CountryRetrieveDestroyView,
     SummaryImageView
 )
 
 urlpatterns = [
     path('countries/refresh', CountryRefreshView.as_view(), name='country-refresh'),
     path('countries/image', SummaryImageView.as_view(), name='countries-image'),
-    path('countries', CountryListView.as_view(), name='countries')
+    path('countries', CountryListView.as_view(), name='countries'),
+    path('countries/<str:name>', CountryRetrieveDestroyView.as_view(), name='country-retrieve-destroy'),
 ]
