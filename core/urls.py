b39be33b3d278c645c15489497112e122f295517
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-10-26 15:13:43 +0100

    feat: add url pattern for the countries endpoint

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from base.views import (
     CountryRefreshView,
+    CountryListView,
     SummaryImageView
 )
 
 urlpatterns = [
     path('countries/refresh', CountryRefreshView.as_view(), name='country-refresh'),
-    path('countries/image', SummaryImageView.as_view(), name='countries-image')
+    path('countries/image', SummaryImageView.as_view(), name='countries-image'),
+    path('countries', CountryListView.as_view(), name='countries')
 ]
